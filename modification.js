b90943995168343cdcbd30bac8903007989b5a96
iDavLaFF <idavlaff@ya.ru>
2025-02-15 19:22:07 +0300

    Update modification.js

--- modification.js
+++ modification.js
@@ -21,7 +21,7 @@
           reactions: true,
           discuss: true,
           ai: true,
-          install_proxy: true,
+          install_proxy: false,
           subscribe: true,
           blacklist: true,
           persons: true,
@@ -35,8 +35,9 @@
 
     // Добавляем плагины
     const plugins = [
-      'https://plugin.rootu.top/tmdb.js', // @ROOTU thanks. TMDB Proxy v1.1.1
-      'https://idavlaff.github.io/plugins/tracks.js', // @CUB thanks for all of your work
+//      'https://plugin.rootu.top/tmdb.js', // @ROOTU thanks. TMDB Proxy v1.1.1
+      'https://cub.red/plugin/tracks', // @CUB thanks.
+//      'https://idavlaff.github.io/plugins/tracks.js', // @CUB tracks plugin backup
       'https://idavlaff.github.io/plugins/timecode.js', // @LAMPAC thanks. !NB Timecode work only in torrents
       'https://idavlaff.github.io/plugins/beautify.js', // @CUB thanks, !'but pay for a big background, seriously?' Combine 'interface' & 'cardify' plugins => 'beautify'. @BYLAMPA thanks, for fix rating issue on card
       'https://lampame.github.io/main/pubtorr/pubtorr.js', // @LME thanks. Public parsers
