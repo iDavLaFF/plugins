c8174268c4098fb10c348e55b90905b4b756d731
iDavLaFF <idavlaff@ya.ru>
2025-03-02 02:00:35 +0300

    revert

--- modification.js
+++ modification.js
@@ -41,9 +41,8 @@
       'https://idavlaff.github.io/plugins/tracks.js', // @CUB tracks plugin backup
       'https://idavlaff.github.io/plugins/timecode.js', // @LAMPAC thanks. !NB Timecode work only in torrents
       'https://idavlaff.github.io/plugins/beautify.js', // @CUB thanks, !'but pay for a big background, seriously?' Combine 'interface' & 'cardify' plugins => 'beautify'. @BYLAMPA thanks, for fix rating issue on card
-      'https://idavlaff.github.io/plugins/cloud.js', // cloud sync bookmarks & history
       'https://lampame.github.io/main/pubtorr/pubtorr.js', // @LME thanks. Public parsers
-//      'https://skaz.tv/export.js', //@SKAZ thanks. Bookmarks & History backup plugin
+      'https://skaz.tv/export.js', //@SKAZ thanks. Bookmarks & History backup plugin
       'https://bwa.to/rc', // @LAMPAC thanks. Online balansers plugin
       'https://ab2024.ru/sisi.js', // @LAMPAC thanks. 18+ balansers plugin. @AB thanks for more stable version of plugin
       'https://idavlaff.github.io/plugins/mult.js' // Add new category "Cartoons"
