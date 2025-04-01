9083826ce0649c3faffb73cb03221e707fd90d43
iDavLaFF <idavlaff@ya.ru>
2025-04-01 21:14:47 +0300

    Update RT.js

--- RT.js
+++ RT.js
@@ -6,7 +6,8 @@
             var cardData = e.object;
             if (e.type == "complite") {
                 var settings = {
-                    url: `https://api.mdblist.com/tmdb/${cardData.method == 'tv' ? 'show' : cardData.method}/${cardData.id}?apikey=7vl42a1fn8uv03qk78ffz7rce`,
+                    url: "https://api.mdblist.com/tmdb/".concat(cardData.method == 'tv' ? 'show' : cardData.method, "/").concat(cardData.id, "?apikey=7vl42a1fn8uv03qk78ffz7rce"),
+//                    url: `https://api.mdblist.com/tmdb/${cardData.method == 'tv' ? 'show' : cardData.method}/${cardData.id}?apikey=7vl42a1fn8uv03qk78ffz7rce`,
                     method: "GET",
                     timeout: 0
                 };
@@ -18,10 +19,10 @@
                                 return rating.source === 'tomatoes' && rating.value !== null;
                             });
 
-                        if (tomatoRating && parseFloat(tomatoRating.value) > 0) {
+                        if (tomatoRating && parseFloat(rating.value) > 0) {
                             var html = Lampa.Template.get('full', cardData.id);
                             if (html) {
-                                html.find('.rate--rt').removeClass('hide').find('> div').eq(0).text(parseFloat(tomatoRating.value) >= 100 ? 100 : tomatoRating.value + '% 🍅');
+                                html.find('.rate--rt').removeClass('hide').find('> div').eq(0).text(parseFloat(rating.value) >= 100 ? 100 : rating.value + '% 🍅');
                             }
                         }
                     }
