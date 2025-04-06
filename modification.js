f36315ca81ba7b31eaeb87328db410282a483182
iDavLaFF <idavlaff@ya.ru>
2025-04-07 01:41:39 +0300

    Update modification.js

--- modification.js
+++ modification.js
@@ -38,7 +38,7 @@
     const plugins = [
       'https://plugin.rootu.top/tmdb.js', // @ROOTU thanks. TMDB Proxy v1.1.1
       'https://idavlaff.github.io/plugins/tracks.js', // @CUB tracks plugin backup
-//      'https://idavlaff.github.io/plugins/beautify.js', // @CUB thanks, !'but pay for a big background, seriously?' Combine 'interface' & 'cardify' plugins => 'beautify'. @BYLAMPA thanks, for fix rating issue on card
+      'https://idavlaff.github.io/plugins/beautify.js', // @CUB thanks, !'but pay for a big background, seriously?' Combine 'interface' & 'cardify' plugins => 'beautify'. @BYLAMPA thanks, for fix rating issue on card
       'https://lampame.github.io/main/pubtorr/pubtorr.js', // @LME thanks. Public parsers
       'https://skaz.tv/export.js', //@SKAZ thanks. Bookmarks & History backup plugin
       'https://bwa.to/rc', // @LAMPAC thanks. Online balansers plugin
@@ -65,7 +65,7 @@
         set: 'true',
         language: 'ru',
         interface_size: 'small',
-        glass_style: 'false',
+        glass_style: 'true',
         advanced_animation: 'false',
         player_timecode: 'continue',
         video_quality_default: '2160',
