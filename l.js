e957a6b78313e5924cc71b9212333a293d46f231
iDavLaFF <idavlaff@ya.ru>
2025-04-14 19:52:25 +0300

    Update l.js

--- l.js
+++ l.js
@@ -1,6 +1,6 @@
 !function() {
     "use strict";
-    
+
     Lampa.SettingsApi.addParam({
         component: "interface",
         param: {
@@ -18,23 +18,51 @@
         }
     });
 
+    // Кэш логотипов
+    window.__logo_cache = window.__logo_cache || {};
+
+    // Предзагрузка логотипа при наведении
+    $('body').on('mouseenter', '.card', function() {
+        if (Lampa.Storage.get("logo_glav") === "1") return;
+
+        var cardData = $(this).data('card-data');
+        if (!cardData || !cardData.id) return;
+
+        var mediaType = cardData.first_air_date ? "tv" : "movie";
+        var id = cardData.id;
+        if (window.__logo_cache[id]) return; // Уже загружено
+
+        var url = Lampa.TMDB.api(`${mediaType}/${id}/images?api_key=${Lampa.TMDB.key()}&language=${Lampa.Storage.get("language")}`);
+        
+        $.get(url, function(response) {
+            if (response.logos && response.logos[0]) {
+                window.__logo_cache[id] = response.logos[0].file_path;
+            } else {
+                window.__logo_cache[id] = null;
+            }
+        });
+    });
+
+    // Подстановка логотипа при открытии карточки
     window.logoplugin || (window.logoplugin = !0, Lampa.Listener.follow("full", function(a) {
-        if ("complite" == a.type && "1" != Lampa.Storage.get("logo_glav")) {
-            var e = a.data.movie,
-                t = Lampa.TMDB.api((e.first_air_date ? "tv" : "movie") + "/" + e.id + "/images?api_key=" + Lampa.TMDB.key() + "&language=" + Lampa.Storage.get("language"));
-            
-            console.log(t);
-            
-            $.get(t, function(e) {
-                if (e.logos && e.logos[0]) {
-                    var t = e.logos[0].file_path;
-                    "" != t && a.object.activity.render().find(".full-start-new__title").html(
-                        '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="' + 
-                        Lampa.TMDB.image("/t/p/w300" + t.replace(".svg", ".png")) + 
-                        '" />'
-                    );
-                }
-            });
+        if (a.type !== "complite" || Lampa.Storage.get("logo_glav") === "1") return;
+
+        var movie = a.data.movie;
+        var mediaType = movie.first_air_date ? "tv" : "movie";
+        var id = movie.id;
+        var logoPath = window.__logo_cache[id];
+
+        var $title = a.object.activity.render().find(".full-start-new__title");
+
+        if (logoPath) {
+            // Используем кэш
+            $title.css({ visibility: "hidden", opacity: 0 });
+            $title.html(
+                '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="' + 
+                Lampa.TMDB.image("/t/p/w500" + logoPath.replace(".svg", ".png")) + 
+                '" />'
+            );
+            $title.css("visibility", "visible").animate({ opacity: 1 }, 300);
         }
     }));
 }();
