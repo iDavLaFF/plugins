fa9d9442821d1e1ddeb6b4570114df05f8e58372
iDavLaFF <idavlaff@ya.ru>
2025-04-17 19:34:33 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -30,19 +30,28 @@
                     var t = e.logos[0].file_path;
                     if ("" != t) {
                         var titleElement = a.object.activity.render().find(".full-start-new__title");
+                        var originalContent = titleElement.html();
 
-                        titleElement.css({'transition': 'all 0.5s ease', 'transform': 'translateY(-20px)', 'opacity': '0', 'pointer-events': 'none'});
-
-                        setTimeout(function() {
-                            titleElement.html('<div class="logo-container" style="overflow: hidden; height: 1.8em; position: relative;">' + 
+                            titleElement.after('<div class="logo-container" style="overflow: hidden; height: 1.8em; position: relative; display: none;">' + 
                                 '<div class="logo-animation" style="position: absolute; width: 100%; transform: translateY(20px); opacity: 0; transition: all 0.5s ease;">' + 
                                 '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em; display: block;" src="' + 
-                                Lampa.TMDB.image("/t/p/w300" + t.replace(".svg", ".png")) + '" />' + '</div>' + '</div>');
+                                Lampa.TMDB.image("/t/p/w500" + t.replace(".svg", ".png")) + '" />' + '</div>' + '</div>');
+
+                            var logoContainer = titleElement.next(".logo-container");
+
+                            titleElement.css({'transition': 'all 0.5s ease', 'transform': 'translateY(-20px)', 'opacity': '0'});
 
                             setTimeout(function() {
-                                titleElement.find(".logo-animation").css({'transform': 'translateY(0)', 'opacity': '1'});
-                            }, 50);
-                        }, 500);
+                                logoContainer.show();
+
+                                setTimeout(function() {
+                                    logoContainer.find(".logo-animation").css({'transform': 'translateY(0)', 'opacity': '1'});
+
+                                    setTimeout(function() {
+                                        titleElement.remove();
+                                    }, 500);
+                                }, 50);
+                            }, 500);
                     }
                 }
             });
