1bc762ca470b61824df3ecbf4774ba35630a9a9f
iDavLaFF <idavlaff@ya.ru>
2025-04-18 01:21:26 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -32,7 +32,7 @@
                         var titleElement = a.object.activity.render().find(".full-start-new__title");
                         var originalContent = titleElement.html();
                         titleElement.after(`
-                        <div class="logo-container" style="overflow: hidden; height: 15em; position: relative; display: none; margin-bottom: 2em;">
+                        <div class="logo-container" style="overflow: hidden; height: 25em; position: relative; display: none; margin-bottom: 2em;">
                         <div class="logo-animation" style="position: absolute; width: 100%; bottom: 0; transform: translateY(20px); opacity: 0; transition: all 0.5s ease;">
                             <img style="display: block; max-width: 80%; height: auto;" src="${Lampa.TMDB.image("/t/p/w500" + t.replace(".svg", ".png"))}"></div></div>`);
                         var logoContainer = titleElement.next(".logo-container");
