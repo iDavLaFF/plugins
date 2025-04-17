caff0309814bda082515887e9b9ba881198ac46d
iDavLaFF <idavlaff@ya.ru>
2025-04-17 19:53:08 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -26,60 +26,27 @@
             console.log(t);
             
             $.get(t, function(e) {
-    if (e.logos && e.logos[0]) {
-        var t = e.logos[0].file_path;
-        if ("" != t) {
-            var titleElement = a.object.activity.render().find(".full-start-new__title");
-            var parentContainer = titleElement.parent();
-            
-            // 1. Анимация исчезновения текста
-            titleElement.css({
-                'transition': 'all 0.5s ease',
-                'transform': 'translateY(-20px)',
-                'opacity': '0'
+                if (e.logos && e.logos[0]) {
+                    var t = e.logos[0].file_path;
+                    if ("" != t) {
+                        var titleElement = a.object.activity.render().find(".full-start-new__title");
+                        var parentContainer = titleElement.parent();
+                        titleElement.css({'transition': 'all 0.5s ease', 'transform': 'translateY(-20px)', 'opacity': '0'});
+                        setTimeout(function() {
+                            parentContainer.append(`
+                            <div class="logo-container" style="overflow: hidden; height: 4em; position: relative; margin-bottom: 0.2em;">
+                            <div class="logo-animation" style="position: relative; width: 100%; transform: translateY(20px); opacity: 0; transition: all 0.5s ease;">
+                                <img style="max-height: 2em; margin-top: 1em; margin-bottom: 0.5em;" src="${Lampa.TMDB.image("/t/p/w500" + t.replace(".svg", ".png"))}"></div></div>`);
+                            setTimeout(function() {
+                                parentContainer.find(".logo-animation").css({'transform': 'translateY(0)', 'opacity': '1'});
+                                setTimeout(function() {
+                                    titleElement.remove();
+                                }, 500);
+                            }, 50);
+                        }, 500);
+                    }
+                }
             });
-            
-            // 2. Создаем логотип в том же контейнере
-            setTimeout(function() {
-                parentContainer.append(`
-                    <div class="logo-container" style="
-                        overflow: hidden; 
-                        height: 4em; 
-                        position: relative;
-                        margin-bottom: 0.2em;
-                    ">
-                        <div class="logo-animation" style="
-                            position: relative;
-                            width: 100%;
-                            transform: translateY(20px);
-                            opacity: 0;
-                            transition: all 0.5s ease;
-                        ">
-                            <img style="
-                                max-height: 2em;
-                                margin-top: 1em;
-                                margin-bottom: 0.5em;
-                            " src="${Lampa.TMDB.image("/t/p/w500" + t.replace(".svg", ".png"))}">
-                        </div>
-                    </div>
-                `);
-                
-                // 3. Анимация появления логотипа
-                setTimeout(function() {
-                    parentContainer.find(".logo-animation").css({
-                        'transform': 'translateY(0)',
-                        'opacity': '1'
-                    });
-                    
-                    // Удаляем оригинальный заголовок после завершения анимации
-                    setTimeout(function() {
-                        titleElement.remove();
-                    }, 500);
-                }, 50);
-            }, 500);
-        }
-    }
-});
         }
     }));
 }();
