e2008c95ff968aa6c8fecc9085b9c26513366d00
iDavLaFF <idavlaff@ya.ru>
2025-04-17 19:17:56 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -31,10 +31,10 @@
                     if ("" != t) {
                         var titleElement = a.object.activity.render().find(".full-start-new__title");
 
-                        titleElement.css({'transition': 'all 0.5s ease', 'transform': 'translateY(-20px)', 'opacity': '0'});
+                        titleElement.css({'transition': 'all 0.5s ease', 'transform': 'translateY(-20px)', 'opacity': '0', 'pointer-events': 'none'});
 
                         setTimeout(function() {
-                            titleElement.html('<div class="logo-container" style="overflow: hidden; height: ' + titleElement.height() + 'px; position: relative;">' + 
+                            titleElement.html('<div class="logo-container" style="overflow: hidden; height: 1.8em; position: relative;">' + 
                                 '<div class="logo-animation" style="position: absolute; width: 100%; transform: translateY(20px); opacity: 0; transition: all 0.5s ease;">' + 
                                 '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em; display: block;" src="' + 
                                 Lampa.TMDB.image("/t/p/w300" + t.replace(".svg", ".png")) + '" />' + '</div>' + '</div>');
